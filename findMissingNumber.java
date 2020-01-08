18e73a376865c43f17c124e01afc004653670c66
Durgesh <2308durgesh@gmail.com>
2020-01-08 21:15:32 +0530

    Update findMissingNumber.java

--- findMissingNumber.java
+++ findMissingNumber.java
@@ -1,16 +1,27 @@
-
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-
-class GFG {
+class missingNumber{
 	public static void main (String[] args) {
-		//code
 		Scanner sc =new Scanner(System.in);
 		int times;
         times=sc.nextInt();
+        while(times>0){
+            int num=0;
+            num=sc.nextInt();
+            int number[] = new int[num];
+            for(int i=1;i<num;i++)
+            {
+                number[i] =sc.nextInt();
+            }
+            int total = (num ) * (num +1) / 2; 
+            for (int i = 0; i < num; i++) {
+                total -= number[i];  
+            }
+            System.out.println(total);
+            times--;
+        }
+        /*
         while(times-- >0){
             int num=0;
             num=sc.nextInt();
@@ -23,6 +34,6 @@ class GFG {
                 temp=temp^i;
             }
             System.out.println(temp^num);
-        }
+        }*/
 	}
 }
